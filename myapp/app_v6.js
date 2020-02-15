9ad68a9a2d2ccfb8649c7ac4c4256e41182e9cc1
lazytosay <siweisijiao.weebly@gmail.com>
2020-02-15 15:26:08 -0800

    take care of the situation when upload no files will crash the program, added upload success and fail page

--- myapp/app_v6.js
+++ myapp/app_v6.js
@@ -8,9 +8,9 @@ const htmlsFolder = path.join(__dirname, "htmlFiles/")
 const version = 6;
 
 //test sending html page
-app.get('/about', function(req, res){
+app.get('/upload', function(req, res){
     //res.sendFile(path.join(__dirname, 'upload_v2.html'));
-    res.sendFile(path.join(htmlsFolder, 'upload_v2.html'));
+    res.sendFile(path.join(htmlsFolder, 'upload.html'));
     //res.render(path.join(__dirname, '/htmlFiles/upload_v2.html'));
     //the second line is not going to show up,
     //so I guess send is like a return
@@ -21,8 +21,14 @@ app.get('/about', function(req, res){
 app.get('/', (req, res) => res.send("hello world, version " + version));
 
 
-app.post('/upload', (req, res)=>{
+app.post('/handleupload', (req, res)=>{
     new formidable.IncomingForm().parse(req)
+
+        //not really working...
+        .on("error", (err) =>{
+            res.sendFile(htmlsFolder + 'uploadFailed.html');
+        })
+
         .on('fileBegin', (name, file) =>{
             file.path = path.join(path.join(__dirname + '/repos/') + file.name);
         })
@@ -31,7 +37,7 @@ app.post('/upload', (req, res)=>{
             console.log("uploaded file: " , name, file);
         })
 
-    res.sendFile(htmlsFolder + 'upload_v2.html');
+    res.sendFile(htmlsFolder + 'uploadSuccess.html');
 
 })
 
