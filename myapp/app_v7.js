5bc83bf04e33993595015646b9d4ff22ce840dd1
lazytosay <siweisijiao.weebly@gmail.com>
2020-02-19 19:37:22 -0800

    in version 7, when user enter the path they want to scan, and submit, it will scaned and return to the browser

---
+++ myapp/app_v7.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+//for dealing with file paths
+const path = require("path");
+
+//for handling upload files..
+const formidable = require("formidable");
+
+const htmlsFolder = path.join(__dirname, "htmlFiles/")
+const version = 6;
+
+//potential code to accept user input
+app.get('/getpathinput', (req, res) =>{
+    res.sendFile(htmlsFolder + "getPathInput.html");
+    //getting user input from the html user input box
+    var userInput = req.query.myInputBox;
+
+    //this line is important, because when first loaded, the code will
+    //wait for user input and code will keep running, which is not wanted
+    if(!userInput){
+        return;
+    }
+
+    console.log("the input: " + userInput);
+
+    //do the scan part
+    var _getAllFilesFromFolder = function(dir) {
+        var filesystem = require("fs");
+        var results = [];
+
+        filesystem.readdirSync(dir).forEach(function(file) {
+
+            file = dir+'/'+file;
+            var stat = filesystem.statSync(file);
+
+            if (stat && stat.isDirectory()) {
+                results = results.concat(_getAllFilesFromFolder(file))
+            } else results.push(file);
+
+        });
+
+        console.log("the results: " , results);
+
+        return results;
+
+    };
+
+    //call the scan function, and get the result list
+    var tempResult = _getAllFilesFromFolder(userInput);
+
+    //FIXME: this is just the temporary solution, should formatted in some way
+    //in html page or so
+    var resultToSend = "<--- Folder Structure List ---->";
+    tempResult.forEach((item) => {
+        resultToSend = resultToSend + item + "\n";
+    });
+
+    res.send("done: " + resultToSend);
+
+
+
+});
+
+//the scan user's folder's part
+app.get('/scan', (req, res) => {
+    //res.sendFile(path.join(htmlsFolder, 'scanFolder.html'));
+
+    ///*
+    var _getAllFilesFromFolder = function(dir) {
+
+        var filesystem = require("fs");
+        var results = [];
+
+        filesystem.readdirSync(dir).forEach(function(file) {
+
+            file = dir+'/'+file;
+            var stat = filesystem.statSync(file);
+
+            if (stat && stat.isDirectory()) {
+                results = results.concat(_getAllFilesFromFolder(file))
+            } else results.push(file);
+
+        });
+
+        console.log("the results: " , results);
+
+        return results;
+
+    };
+
+    //var userPath = prompt("please enter your file path: ");
+    //console.log("the user path: " , userPath);
+
+    //_getAllFilesFromFolder(userPath);
+    //hard code now, need user input later
+    //var tempResult = _getAllFilesFromFolder("C:\\Users\\siwei\\Desktop\\college_stuff\\GitHub\\helloWorld\\CECS343_VersionControl\\myapp\\repos");
+    var tempResult = _getAllFilesFromFolder("C:\\Users\\siwei\\Desktop\\college_stuff\\GitHub\\helloWorld\\CECS343_VersionControl");
+
+    console.log("Done with scan");
+
+    var resultToSend = "\nFolder Structure List ---->";
+    tempResult.forEach((item) => {
+        resultToSend = resultToSend + item + "\n";
+    });
+
+    res.send("done: " + resultToSend);
+    //*/
+})
+
+//root
+app.get('/', (req, res) => res.send("hello world, version " + version));
+
+//the url that user can upload file(s)
+// http://localhost:3000/upload
+app.get('/upload', function(req, res){
+    res.sendFile(path.join(htmlsFolder, 'upload.html'));
+});
+
+
+//the part that will be activated to accpet any upload when user upload
+app.post('/handleupload', (req, res)=>{
+    new formidable.IncomingForm().parse(req)
+
+        //error catching not really working...
+        .on("error", (err) =>{
+            res.sendFile(htmlsFolder + 'uploadFailed.html');
+        })
+
+        .on('fileBegin', (name, file) =>{
+            file.path = path.join(path.join(__dirname + '/repos/') + file.name);
+        })
+
+        .on('file', (name, file) =>{
+            console.log("uploaded file: " , name, file);
+        })
+
+    res.sendFile(htmlsFolder + 'uploadSuccess.html');
+
+})
+
+
+
+
+//don't remove this line, it keeps the the listening to the port, not let the app to end
+app.listen(port, () => console.log(`version ${version}: listen to port : ${port}`));
