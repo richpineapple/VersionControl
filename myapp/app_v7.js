0a92c6c05adb8f6211d03248280e7b8b22f09bd5
lazytosay <siweisijiao.weebly@gmail.com>
2020-02-24 21:15:45 -0800

    put it back

--- myapp/app_v7.js
+++ myapp/app_v7.js
@@ -126,6 +126,9 @@ var _getAllFilesFromFolder = function(dir) {
 
 };
 
+app.get('/main', function(req, res){
+    res.sendFile(path.join(htmlsFolder, 'MainPage.html'));
+});
 
 //don't remove this line, it keeps the the listening to the port, not let the app to end
 app.listen(port, () => console.log(`version ${version}: listen to port : ${port}`));
