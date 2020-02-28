0b1a42c5df8fa3bb84f771ee0e3d6e1bbf7bb9c7
Donovan Lee <32886721+LeeDonovan@users.noreply.github.com>
2020-02-28 14:34:00 -0800

    fix

--- myapp/app_v7.js
+++ myapp/app_v7.js
@@ -129,7 +129,31 @@ app.get('/commit', (req, res) =>{
         filesystem.writeFileSync(path.join(repoPath, artID), content);
 
             //step 5: save to the manifest file
-        //}
+            var location = path.join(__dirname + "/repos/.manifest.txt");
+        
+            var today = new Date().toLocaleDateString(undefined,{
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+            })
+    
+            //checks if manifest file exists
+            filesystem.access(location, filesystem.F_OK, (err) =>{
+                if(err)
+                {
+                    console.log("Something is wrong");
+                    //if it does not create it
+                    filesystem.writeFileSync(location, artID + "\t"+ relativePathStr+"\t"+"Commit\t"+today+"\n");
+                    console.log("Creating file....");
+                    return;
+                }
+                filesystem.appendFile(location, artID + "\t"+ relativePathStr+"\t"+"Commit\t"+today+"\n", function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                    });
+            })
 
     });
 
