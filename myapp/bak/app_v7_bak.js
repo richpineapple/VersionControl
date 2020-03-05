5b3cfe40c4122524e3b52eb5246b54b91dfa39ce
lazytosay <siweisijiao.weebly@gmail.com>
2020-03-04 21:15:00 -0800

    detele some unnecessary codes

--- myapp/bak/app_v7_bak.js
+++ myapp/bak/app_v7_bak.js
@@ -15,10 +15,6 @@ const repoPath = path.join(__dirname + "/repos/");
 const version = 7;
 
 
-app.get('createRepo', (req, res)=>{
-
-});
-
 //now we are able to read the files in certain path, then saving them should not be a problem
 app.get('/commit', (req, res) =>{
     //date and time for names
@@ -208,6 +204,60 @@ var copyFileTo = function(from, to){
 };
 
 
+//potential code to accept user input
+app.get('/getpathinput', (req, res) =>{
+    res.sendFile(htmlsFolder + "getPathInput.html");
+    //getting user input from the html user input box
+    var userInput = req.query.myInputBox;
+
+    //this line is important, because when first loaded, the code will
+    //wait for user input and code will keep running, which is not wanted
+    if(!userInput){
+        return;
+    }
+
+    //fixme delete later
+    console.log("the input: " + userInput);
+
+    //call the scan function, and get the result list
+    var tempResult = _getAllFilesFromFolder(userInput);
+
+    //FIXME: because of the user form, I can not send another html file..
+    res.sendFile(htmlsFolder + 'scanCompleted.html');
+    /*
+    if(!tempResult.length){
+        console.log("the length: " + tempResult.length);
+        //res.sendFile(htmlsFolder + "inputError.html");
+        res.sendFile(htmlsFolder + 'uploadSuccess.html');
+        //res.send("wrong input..");
+        return;
+    }
+
+    console.log("result: " + tempResult);
+
+    //FIXME: this is just the temporary solution, should formatted in some way
+    //in html page or so
+    var scanResult = "";
+    tempResult.forEach((item) => {
+        scanResult = scanResult + item + "\n";
+    });
+
+    //save it to server
+    //scanResult will be used to upload the files in that location later
+    filesystem.writeFile(helperFilesFolder + "scanResult.txt", scanResult,function(err){
+        if (err) throw err;
+    });
+
+    //FIXME: need to check if repeat
+    //the paathToScan.txt is used to save the paths user requests for scanning
+    filesystem.writeFile(helperFilesFolder + "pathToScan.txt", userInput ,function(err){
+        if (err) throw err;
+    });
+
+
+    res.send("done: " + scanResult);
+    */
+});
 
 
 //root
@@ -215,6 +265,33 @@ app.get('/', function(req, res){
     res.sendFile(path.join(htmlsFolder, 'MainPage.html'));
 });
 
+//the url that user can upload file(s)
+// http://localhost:3000/upload
+app.get('/upload', function(req, res){
+    res.sendFile(path.join(htmlsFolder, 'upload.html'));
+});
+
+
+//the part that will be activated to accpet any upload when user upload
+app.post('/handleupload', (req, res)=>{
+    new formidable.IncomingForm().parse(req)
+
+        //error catching not really working...
+        .on("error", (err) =>{
+            res.sendFile(htmlsFolder + 'uploadFailed.html');
+        })
+
+        .on('fileBegin', (name, file) =>{
+            file.path = path.join(path.join(__dirname + '/repos/') + file.name);
+        })
+
+        .on('file', (name, file) =>{
+            console.log("uploaded file: " , name, file);
+        })
+
+    res.sendFile(htmlsFolder + 'uploadSuccess.html');
+
+});
 
 var getAllBaseName = function(dir){
     var allFilesPath = _getAllFilesFromFolder(dir);
