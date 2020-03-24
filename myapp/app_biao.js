9f432c370c1d1a0f6606776db5a09cb65e8b5e36
lazytosay <siweisijiao.weebly@gmail.com>
2020-03-23 20:34:35 -0700

    sample template

--- myapp/app_biao.js
+++ myapp/app_biao.js
@@ -18,6 +18,32 @@ const filesystem = require("fs");
 const htmlsFolder = path.join(__dirname, "htmlFiles/");
 const version = 8;
 
+
+//template part
+app.set('view engine', 'ejs');
+var ejs = require('ejs');
+
+var students = [
+     'Mark',
+     'Tom',
+     'John'
+]
+app.get('/test', function(req, res){
+
+
+    //change repo path to the actual repo path
+    var repoPath = __dirname;
+
+    //only want the name of the files, not the path
+    var baseNames = getAllBaseName(repoPath);
+
+    //the "students.ejs" should already be in the views folder
+    res.render('students', {students: baseNames});
+
+});
+
+
+
 //copy files according the .man file
 app.get('/checkout', (req, res) =>{
     //now we get 2 user input
