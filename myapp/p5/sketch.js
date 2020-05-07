c6d45cf9c31a329a6765ce875fed1b1543e6b9ed
Donovan Lee <32886721+LeeDonovan@users.noreply.github.com>
2020-05-07 14:29:19 -0700

    p5_update

--- myapp/p5/sketch.js
+++ myapp/p5/sketch.js
@@ -1,18 +1,34 @@
+//if you want to test this out look into the MainPage.html
 var canvas;
 var h1;
 var change;
-var x = 100;
-var y = 200;
+var x = 5;
+var y = 150;
+var xDirection = 1;
+var xSpeed = 3;
+var button;
+var input;
+var nameP;
+var createRepo;
 
 function setup() {
-  canvas = createCanvas(400, 400);
+  canvas = createCanvas(displayWidth, displayHeight);
   canvas.position(400,500);
+  button = createButton("Change the Text");
+  createRepo = createButton("Create Repo Folder");
+  button.mousePressed(textChanger);
+  input = createInput("Type your name here");
+  nameP = createP("Your Name!");
   h1 = createElement('h1', 'Click On the Page to see an Easter Egg');
-  //h1.position(400,600);
+  h1.position(x,y);
   change = 0;
 }
 
-function mousePressed(){
+function repoCreater(){
+  
+}
+
+function textChanger(){
   if(change == 0){
     h1.html("Hello There");
   }
@@ -26,10 +42,18 @@ function mousePressed(){
 }
 
 function draw() {
-  clear();
   background(200);
   fill(255,0,0);
   rect(100,100,50,50);
+  nameP.html(input.value());
+  text(input.value(),10,20);
+  x = x + xSpeed * xDirection;
+  if(x > 400){
+    xDirection*=-1;
+  }
+  if(x < 5){
+    xDirection *=-1;
+  }
   h1.position(x,y);
   
 }
